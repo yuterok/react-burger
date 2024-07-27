f4e20d6ac0e161ce76c11eacac106d74debd8050
Julia Tereschenko <yuterok@gmail.com>
2024-07-28 01:57:00 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from '@ya.praktikum/react-developer-burger-ui-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button htmlType="button" type="primary" size="small" extraClass="ml-2">
+  Нажми на меня
+</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
