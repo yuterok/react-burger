aab737219285b6b8bb836219e3e0f3e686234f6a
Julia Tereschenko <yuterok@gmail.com>
2024-10-12 18:37:13 +0300

    fixes after review (cypress code optimization)

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -1,52 +1,57 @@
+const selectors = {
+  ingredient: '[data-cy="ingredient"]',
+  constructor: '[data-cy="burger-constructor"]',
+  modal: '[data-cy="modal"]',
+};
+
 describe("testing Burger Constructor", () => {
   beforeEach(() => {
     let email = "yuterok@mail.ru";
     let password = "pass";
-    cy.visit("http://localhost:3000/login");
+    cy.visit("/login");
     cy.get("[type=email]").type(`${email}{enter}`);
     cy.get("[type=password]").type(`${password}{enter}`);
   });
 
   it("should drag an ingredient into constructor", () => {
-    cy.get('[data-cy="ingredient"]').first().as("ingredient");
-    cy.get('[data-cy="burger-constructor"]').as("constructor");
+    cy.get(selectors.ingredient).first().as("ingredient");
     cy.get("@ingredient").trigger("dragstart");
-    cy.get("@constructor").trigger("drop");
-    cy.get('[data-cy="burger-constructor"]')
+    cy.get(selectors.constructor).trigger("drop");
+    cy.get(selectors.constructor)
       .find(".constructor-element")
       .should("have.length.greaterThan", 0);
   });
   it("should create an order", () => {
-    cy.get('[data-cy="burger-constructor"]').as("constructor");
-    cy.get('[data-cy="ingredient"]').contains("булка").trigger("dragstart");
-    cy.get('[data-cy="burger-constructor"]').trigger("drop");
-    cy.get('[data-cy="ingredient"]').contains("Соус").trigger("dragstart");
-    cy.get('[data-cy="burger-constructor"]').trigger("drop");
+    cy.get(selectors.ingredient).as("ingredient");
+    cy.get(selectors.ingredient).contains("булка").trigger("dragstart");
+    cy.get(selectors.constructor).trigger("drop");
+    cy.get(selectors.ingredient).contains("Соус").trigger("dragstart");
+    cy.get(selectors.constructor).trigger("drop");
     cy.get('[data-cy="makeOrder"]').click();
-    cy.get('[data-cy="modal"]', { timeout: 5000 }).should("be.visible");
+    cy.get(selectors.modal, { timeout: 5000 }).should("be.visible");
     cy.get('[data-cy="order-details"]', { timeout: 20000 }).should(
       "contain.text",
       "идентификатор заказа"
     );
     cy.get('[data-cy="close-modal"]').find("svg").click();
-    cy.get('[data-cy="modal"]').should("not.exist");
+    cy.get(selectors.modal).should("not.exist");
   });
   it("should close modal by Escape", () => {
-    cy.get('[data-cy="ingredient"]').first().as("ingredient").click();
-    cy.get('[data-cy="modal"]', { timeout: 1000 }).should("be.visible");
+    cy.get(selectors.ingredient).first().click();
+    cy.get(selectors.modal, { timeout: 1000 }).should("be.visible");
     cy.get("body").type("{esc}");
-    cy.get('[data-cy="modal"]').should("not.exist");
+    cy.get(selectors.modal).should("not.exist");
   });
   it("should close modal by pressing button", () => {
-    cy.get('[data-cy="ingredient"]').first().as("ingredient").click();
-    cy.get('[data-cy="modal"]', { timeout: 1000 }).should("be.visible");
+    cy.get(selectors.ingredient).first().click();
+    cy.get(selectors.modal, { timeout: 1000 }).should("be.visible");
     cy.get('[data-cy="close-modal"]').find("svg").click();
-    cy.get('[data-cy="modal"]').should("not.exist");
+    cy.get(selectors.modal).should("not.exist");
   });
   it("should close modal by clicking anywhere", () => {
-    cy.get('[data-cy="ingredient"]').first().as("ingredient").click();
-    cy.get('[data-cy="modal"]', { timeout: 1000 }).should("be.visible");
+    cy.get(selectors.ingredient).first().click();
+    cy.get(selectors.modal, { timeout: 1000 }).should("be.visible");
     cy.get("body").click(0, 0);
-    cy.get('[data-cy="modal"]').should("not.exist");
+    cy.get(selectors.modal).should("not.exist");
   });
 });
